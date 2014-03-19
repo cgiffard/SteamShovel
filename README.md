24c073ecb80d53b1f875c640a25adf47d65b1b9f
Christopher Giffard <christopher.giffard@cgiffard.com>
2014-03-19 16:12:02 +1100

    Mostly functional.

--- README.md
+++ README.md
@@ -2,3 +2,27 @@ SteamShovel
 ===========
 
 JS code coverage done right.
+
+### Fast
+
+```sh
+npm run-script coverage
+```
+
+### In detail
+
+```sh
+// Instrument code
+./lib/cli.js inDir outDir
+
+// Use the reporter with mocha
+mocha -R $(pwd)/reporter.js
+```
+
+### Should I use it
+
+No.
+
+### Why
+
+It doesn't work properly yet.
--- test/main.js
+++ test/main.js
@@ -38,26 +38,83 @@ describe("Instrumentor", function() {
 			.indexOf("hijklmnop").should.be.greaterThan(0);
 	});
 
-	it("should not break CallExpressions with MemberExpression callees",
-		function() {
-
-		instrument("abc.def('abc')")
-			.indexOf("abc.def)").should.equal(-1);
-	});
-
-	it("should not replace the left side of AssignmentExpressions",
-		function() {
-
-		instrument("var abc = def;")
-			.indexOf("var abc").should.be.greaterThan(0);
-	});
-
 	it("should return accurate loc and range info in map", function() {
 		var result = instrument("var abc = ('def' || 'abc');");
 
 		result.indexOf('"line":1,"column":11').should.be.greaterThan(0);
 		result.indexOf('"line":1,"column":25').should.be.greaterThan(0);
 		result.indexOf('[11,25]').should.be.greaterThan(0);
+
+	});
+
+	describe("Edge Cases", function() {
+
+		it("should not break CallExpressions with MemberExpression callees",
+			function() {
+
+			instrument("abc.def('abc')", null, false)
+				.indexOf("abc.def)").should.equal(-1);
+		});
+
+		it("should not replace the left side of AssignmentExpressions",
+			function() {
+
+			instrument("var abc = def;", null, false)
+				.indexOf("var abc").should.equal(0);
+		});
+
+		it("should not instrument the components of MemberExpressions",
+			function() {
+
+			var code =
+				"module.exports = function instrumentCode(data, filename) {};";
+
+			instrument(code, null, false)
+				.indexOf("module.exports = ").should.be.greaterThan(-1);
+
+		});
+
+		it("should not break Functions into their components",
+			function() {
+
+			var code =
+				"module.exports = function instrumentCode(data, filename) {};",
+				code2 = "function instrumentCode(data, filename) {};";
+
+			// FunctionExpression
+
+			instrument(code, null, false)
+				.indexOf("function instrumentCode").should.be.greaterThan(-1);
+
+			instrument(code, null, false)
+				.indexOf("data, filename").should.be.greaterThan(-1);
+
+			// FunctionDeclaration
+
+			instrument(code2, null, false)
+				.indexOf("function instrumentCode").should.be.greaterThan(-1);
+
+			instrument(code2, null, false)
+				.indexOf("data, filename").should.be.greaterThan(-1);
+		});
+
+		it("should not touch the arguments to UpdateExpressions",
+			function() {
+
+			var code = "abc.def ++",
+				code2 = "++ abc.def";
+
+			// Infix
+			
+			instrument(code, null, false)
+				.indexOf("abc.def++").should.be.greaterThan(-1);
+
+			// Prefix
+
+			instrument(code2, null, false)
+				.indexOf("++abc.def").should.be.greaterThan(-1);
+
+		});
 
 	});
 
